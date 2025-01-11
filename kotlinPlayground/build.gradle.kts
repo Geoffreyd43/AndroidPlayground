ead0ef5deceb881d9b716059d3e865da42f64f6c
Geoffrey <geoff.dodd43@gmail.com>
2025-01-11 12:01:41 -0500

    Configure kotlinPlayground to run with gradle run task

--- kotlinPlayground/build.gradle.kts
+++ kotlinPlayground/build.gradle.kts
@@ -1,9 +1,18 @@
 plugins {
     id("java-library")
     alias(libs.plugins.jetbrains.kotlin.jvm)
+    application
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
+}
+
+sourceSets.main {
+    java.srcDirs("src/main/kotlin")
+}
+
+application {
+    mainClass.set("com.example.kotlinplayground.PlaygroundKt")
 }
